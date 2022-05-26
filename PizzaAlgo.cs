dadd564e89663faf685f00b851848d092b07ddd9
Devin Carlisle <devincarlisle1994@yahoo.com>
2022-05-26 10:56:46 -0500

    init commit

---
+++ PizzaAlgo.cs
@@ -0,0 +1,8 @@
+PizzaAlgo
+Ordering from Mellow Mushrooms
+Large 2 Toppping Pizza
+Peperoni and Sausage
+Side of Lemon Pepper Wings
+5a. Messed up my Order
+6a. Called the resturant upset 
+7a. they refused to make changes
